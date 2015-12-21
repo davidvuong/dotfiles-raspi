45f564e63dc4f0dceacb405fc82ee85520b03223
David Vuong <david.vuong256@gmail.com>
2015-12-22 01:12:22 +1100

    fix(mount): fixed unmount errors

--- mount.sh
+++ mount.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo umount /dev/sdb1 /media/pi/Elements
-sudo umount /dev/sda1 /media/pi/SeagateExp
+sudo umount /dev/sdb1
+sudo umount /dev/sda1
 
-sudo mount /dev/sdb1 /media/hdd_1
-sudo mount /dev/sda1 /media/hdd_2
+sudo mount /dev/sda1 /media/hdd_1
+sudo mount /dev/sdb1 /media/hdd_2
