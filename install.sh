8fb505b2eb1a5950e70220c9179d2f9b497be714
David Vuong <david.vuong256@gmail.com>
2015-12-21 22:16:43 +1100

    docs(install.sh): make install script a bit more informative

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+
 sudo apt-get install zsh
 sudo apt-get install vim
 sudo apt-get install htop
 sudo apt-get install ntfs-3g
 sudo apt-get install samba samba-common-bin
 
+# Install and configure ZSH (using my custom Prezto fork).
 zsh
 git clone --recursive https://github.com/davidvuong/prezto "${ZDOTDIR:-$HOME}/.zprezto"
 
@@ -17,12 +19,14 @@ ln -s ~/.zprezto/zsh_nocorrect ~/zsh_nocorrect
 chsh -s /bin/zsh
 source ~/.zshrc
 
+# Use my standard vim bindings, configurating and addons.
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
 
+# Install Python related modules.
 pip install --upgrade pip
 sudo pip install -r ~/dotfiles/requirements.txt
 
@@ -30,6 +34,7 @@ ln -s ~/dotfiles/dircolors ~/.dircolors
 ln -s ~/dotfiles/git/gitignore ~/.gitignore
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 
+# Configure Samba server my custom rules.
 sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
 sudo ln -s ~/dotfiles/smb.conf /etc/samba/smb.conf
 
