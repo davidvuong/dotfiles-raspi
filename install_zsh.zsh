25e9173a96a66e7bf301d183677d5ca64fbadbb1
David Vuong <david.vuong256@gmail.com>
2016-04-17 20:50:21 +1000

    fix(install): rm zprezto if already exists before cloning

--- install_zsh.zsh
+++ install_zsh.zsh
@@ -1,14 +1,17 @@
 #!/bin/zsh
+if [ -d "${ZDOTDIR:-$HOME/.zprezto}" ]; then
+  rm -rf "${ZDOTDIR:-$HOME/.zprezto}";
+fi
 
 # Install and configure ZSH (using my custom Prezto fork).
 git clone --recursive https://github.com/davidvuong/prezto "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-ln -s ~/.zprezto/zsh_nocorrect ~/zsh_nocorrect
+ln -sf ~/.zprezto/zsh_nocorrect ~/zsh_nocorrect
 
 # Change the default login shell to be zsh.
 chsh -s /bin/zsh
